4756fec3e53dcf5aeb241725bdb3f7ca760b5353
Diegocod <diegotiza@hotmail.com>
2021-05-09 22:06:31 -0300

    Configurado para listar registros.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = Post::get();
+
+
+
+        return view('admin.posts.index', [ /* ou usar a função com: compact('posts'); */
+            'posts' => $posts
+        ]);
+    }
 }
